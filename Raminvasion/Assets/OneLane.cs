3cec03102a42d27a68ffa5e1be45506952fefb38
XYremesher <kaankocak12@gmail.com>
2023-05-15 14:39:02 +0200

    Extendable Lane

--- Raminvasion/Assets/OneLane.cs
+++ Raminvasion/Assets/OneLane.cs
@@ -8,36 +8,22 @@ public class OneLane : MonoBehaviour
     public int arrayDepth = 30;
     public float cubeSpacing = 1.0f; // new variable for cube spacing
 
-    private void Start()
+    private void Update()
     {
-        // Add ContinueLanes component to col object
-        ContinueLanes continueLanes = col.AddComponent<ContinueLanes>();
-    }
-    void Update()
-    {
+
         for (int i = 0; i < arrayWidth; i++)
+        {
             for (int j = 0; j < arrayDepth; j++)
             {
-                Instantiate(box, new Vector3(i * cubeSpacing,0, j * cubeSpacing), Quaternion.identity);
-                if(i == arrayWidth)
-                    Instantiate(col, new Vector3(i * cubeSpacing, 0, j * cubeSpacing), Quaternion.identity);
-
-            }
-    }
-
-    public class ContinueLanes : MonoBehaviour
-    {
-        private int addArrayDepth=5;
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.CompareTag("Player"))
-            {
-                int oneLane = GameObject.FindObjectOfType<OneLane>().arrayDepth;
-                oneLane = +addArrayDepth;
-
+                if (j < arrayDepth)
+                {
+                    Instantiate(box, new Vector3(i * cubeSpacing, 0, j * cubeSpacing), Quaternion.identity);
+                    col.transform.position = new Vector3(0, 1, j * cubeSpacing);
+                }
 
             }
         }
     }
+
 }
 
