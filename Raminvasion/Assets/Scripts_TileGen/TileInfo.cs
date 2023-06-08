31e670682db2d6424b47d0051b2ed691b8748116
Jules Podlipensky <julia.podlipensky@stud.h-da.de>
2023-06-09 00:08:13 +0200

    Intorduced Area in TileInformation && small changes
    - Tile as Part of Area MainPath, SecondaryPath, DeadEnd(last tile itself)
    
    - still need to work out where best timing for distribution on area is, bcs actual placement is happening after chopping into single lines

--- Raminvasion/Assets/Scripts_TileGen/TileInfo.cs
+++ Raminvasion/Assets/Scripts_TileGen/TileInfo.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public enum TileArea{
+    MainPath,
+    SecondaryPath,
+    DeadEnd
+}
 
 public enum TileDirection{ 
     Vertical, Horizontal,
